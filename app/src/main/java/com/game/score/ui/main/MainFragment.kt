4c927b8ee9a6955e14d947202a332fffd7ee8d46
starstar fire <starstarfire@outlook.com>
2020-07-25 21:34:10 +0800

    .

--- app/src/main/java/com/game/score/ui/main/MainFragment.kt
+++ app/src/main/java/com/game/score/ui/main/MainFragment.kt
@@ -139,23 +139,11 @@ class MainFragment : Fragment() {
                         Toast.makeText(context, "剩余电量:$level%", Toast.LENGTH_SHORT).show()
                     }
 
-
+                    //region 读取配置测试
                     val pref = activity?.getSharedPreferences(
                         activity?.packageName + "_preferences",
                         Context.MODE_PRIVATE
                     )
-
-//                    val preferences = activity?.getSharedPreferences(
-//                        SettingsFragment.SETTINGS_SHARED_PREFERENCES_FILE_NAME,
-//                        Context.MODE_PRIVATE
-//                    )
-
-                    val aa = getString(R.string.settings_network_local_port_key)
-                    val aa2 = pref?.getString(
-                        getString(R.string.settings_network_local_port_key),
-                        "111"
-                    )
-
 
                     val a1 =
                         pref?.getString(getString(R.string.settings_network_local_port_key), "8080")
@@ -167,13 +155,14 @@ class MainFragment : Fragment() {
                             getString(R.string.settings_network_server_port_key),
                             "8080"
                         )?.toIntOrNull() ?: 8080
-                    val a4 = pref?.getInt(getString(R.string.settings_client_id_key), 1)
+                    val a4 = pref?.getString(getString(R.string.settings_client_id_key), "1")
+                        ?.toIntOrNull() ?: 1
 
                     Log.d("Setting", "settings_network_local_port_key " + a1)
                     Log.d("Setting", "settings_network_server_host_key " + a2)
                     Log.d("Setting", "settings_network_server_port_key " + a3)
                     Log.d("Setting", "settings_client_id_key " + a4)
-                    Log.d("Setting", "settings_client_id_key " + aa2)
+                    //endregion
 
                 }
                 R.id.button_V -> {
