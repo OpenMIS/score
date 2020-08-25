5e4ac13e2fff9e9cb19934056e06e5eb4b02004c
starstar fire <starstarfire@outlook.com>
2020-08-25 14:42:35 +0800

    .

--- app/src/main/java/com/game/score/core/ToastHelper.kt
+++ app/src/main/java/com/game/score/core/ToastHelper.kt
@@ -5,7 +5,7 @@ import android.widget.Toast
 
 object ToastHelper {
     fun setPosition(toast: Toast): Toast =
-        toast.apply { setGravity(Gravity.CENTER, 0, -40) }
+        toast.apply { setGravity(Gravity.CENTER, 0, -45) }
 }
 
 //region 扩展方法
