7bf4ca7966eb20b0da9a8dc888ed53f9083f9e77
starstar fire <starstarfire@outlook.com>
2020-08-25 21:56:33 +0800

    .

--- app/src/main/java/com/game/score/GameService.kt
+++ app/src/main/java/com/game/score/GameService.kt
@@ -98,8 +98,8 @@ class GameService : Service() {
                         )
                     }
                 }
-                //endregion
             }
+            //endregion
         }
     }
 
